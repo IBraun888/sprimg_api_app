bc1ddb024b6528cb0d1f186b98745c788f1e5355
nikolajsmirnov <n.smirnov@autodoc.eu>
2023-10-29 00:02:52 +0300

    upd class HelloWorldController

--- src/main/kotlin/sptring_api/sprimg_api_app/HelloWorldController.kt
+++ src/main/kotlin/sptring_api/sprimg_api_app/HelloWorldController.kt
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/hello")
 class HelloWorldController {
     @GetMapping
-    fun helloWorld(): String = "Hello World Api"
+    fun helloWorld(): String = "Hello World Api my PRO"
 }
