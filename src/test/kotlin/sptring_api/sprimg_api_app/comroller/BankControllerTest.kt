5fb69997c96470fa9a3b6b70d10bf89d54170a32
nikolajsmirnov <n.smirnov@autodoc.eu>
2023-10-29 20:38:51 +0200

    upd

--- src/test/kotlin/sptring_api/sprimg_api_app/comroller/BankControllerTest.kt
+++ src/test/kotlin/sptring_api/sprimg_api_app/comroller/BankControllerTest.kt
@@ -1,11 +1,7 @@
 package sptring_api.sprimg_api_app.comroller
 
 
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -16,32 +12,25 @@ import org.springframework.test.web.servlet.get
 
 @SpringBootTest
 @AutoConfigureMockMvc
-internal class BankControllerTest {
+class TestBankController {
 
-    @Autowired
+
     lateinit var mockMvc: MockMvc
 
 
-    @Nested
-    @DisplayName("getBanks()")
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-
-    inner class GetBanks {
-        @Test
-        fun `should return all banks`() {
-            mockMvc.get("/api/banks")
-                .andDo {
-                    print()
+    @Test
+    public fun `should return all banks`() {
+        mockMvc.get("/api/banks")
+            .andDo {
+                print()
+            }
+            .andExpect {
+                status {
+                    isOk()
                 }
-                .andExpect {
-                    status {
-                        isOk()
-                    }
-                    content { contentType(MediaType.APPLICATION_JSON) }
-                    jsonPath("$[0].accountNumber") { value("1212") }
-                }
-        }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$[0].accountNumber") { value("1212") }
+            }
     }
-
 
 }
